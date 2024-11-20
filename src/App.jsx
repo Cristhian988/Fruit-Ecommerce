6ec27224315941b1c013706da5dba2ab1b689372
Criss <crissfx28@gmail.com>
2024-11-19 21:03:48 -0500

    Fruit Ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero/Hero";
+import Menu from "./components/Menu/Menu";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Menu />
+    </>
+  );
+}
+
+export default App;
